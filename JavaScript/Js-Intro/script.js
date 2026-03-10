98f5c8c3a4b9ecacfe6634bf2aa7433ab11eb9f3
vishnuvardhan791 <vishnuvardhan35251@gmail.com>
2026-03-10 22:38:55 +0530

    JS Basic Intro

---
+++ JavaScript/Js-Intro/script.js
@@ -0,0 +1,8 @@
+console.log("Hello Bro this is an example of External css");
+console.log("we created script.js file seperatly and then we imported using script tag");
+
+// Js is Case-sensitive
+// eg:- let x=5;
+//let X=10;
+// console.log(x);  --> 5
+// console.log(X); --->10
